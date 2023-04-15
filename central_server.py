b0df0df22336b994804338384113037ed0cc0ab1
aldab <ab.barnawi@gmail.com>
2023-04-16 00:39:50 +0300

    initial commit

---
+++ central_server.py
@@ -0,0 +1,54 @@
+###############################################################################
+# (c) Copyright, Real-Time Innovations, 2019.  All rights reserved.           #
+# No duplications, whole or partial, manual or electronic, may be made        #
+# without express written permission.  Any such copies, or revisions thereof, #
+# must display this notice unaltered.                                         #
+# This code contains trade secrets of Real-Time Innovations, Inc.             #
+###############################################################################
+
+"""Reads Squares, transforms them and writes them as Circles."""
+
+from sys import path as sys_path
+from os import path as os_path
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+def init_logger():
+    logger = logging.getLogger("sensor_data.log")
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        fmt='%(asctime)s ~ %(levelname)s ~ %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+
+    fh = TimedRotatingFileHandler("sensor_data.log", when="M", interval=5,backupCount=7)
+    fh.setFormatter(formatter)
+    fh.suffix = "%Y-%m-%d_%H-%M-%S"
+    logger.addHandler(fh)
+    return logger
+
+
+
+file_path = os_path.dirname(os_path.realpath(__file__))
+sys_path.append(file_path + "/../../../")
+import rticonnextdds_connector as rti
+
+logger = init_logger()
+with rti.open_connector(
+        config_name="MyParticipantLibrary::CentralServerParticipant",
+        url=file_path + "/../VitalSigns.xml") as connector:
+
+    input = connector.get_input("CentralServerSubscriber::VitalSign")
+    output = connector.get_output("CentralServerPublisher::VitalSign")
+
+    # Read data from the input, transform it and write it into the output
+    print("Waiting for data...")
+    while True:
+        input.wait() # Wait for data in the input
+        input.take()
+        for sample in input.samples.valid_data_iter:
+            data : dict() = sample.get_dictionary()
+            logger.info(str(data))
+            print("Reveiced {}".format(str(data)))
+            output.instance.set_dictionary(data)
+            output.write()
+
---
+++ sensor.py
@@ -0,0 +1,49 @@
+###############################################################################
+# (c) 2005-2015 Copyright, Real-Time Innovations.  All rights reserved.       #
+# No duplications, whole or partial, manual or electronic, may be made        #
+# without express written permission.  Any such copies, or revisions thereof, #
+# must display this notice unaltered.                                         #
+# This code contains trade secrets of Real-Time Innovations, Inc.             #
+###############################################################################
+
+from time import sleep
+
+# Updating the system path is not required if you have pip-installed
+# rticonnextdds-connector
+from sys import path as sys_path
+from sys import argv
+from os import path as os_path
+from random import randint
+import time
+file_path = os_path.dirname(os_path.realpath(__file__))
+sys_path.append(file_path + "/../../../")
+
+import rticonnextdds_connector as rti
+
+if len(argv) != 2 and  not isinstance(argv[1],int) :
+    print("Please run as follows: python senser.py <patient_id>")
+    exit(-1)
+id = int(argv[1])
+
+with rti.open_connector(
+        config_name="MyParticipantLibrary::SensorParticipant",
+        url=file_path + "/../VitalSigns.xml") as connector:
+
+    output = connector.get_output("SensorPublisher::VitalSign")
+
+    print("Waiting for subscriptions...")
+    output.wait_for_subscriptions()
+
+    print("Writing...")
+    i=0
+    while True:
+        output.instance.set_number("hr",randint(60,100))
+        output.instance.set_number("bp", randint(60,140))
+        output.instance.set_number("o", randint(95,100))
+        output.instance.set_number("id", id)
+        output.instance.set_number("count", i)
+        output.instance.set_number("timestamp",time.time())
+        output.write()
+
+        sleep(0.5) # Write at a rate of one sample every 0.5 seconds, for ex.
+        i+=1
